7c38eac20cbb99e51d87c483169c3ecb5e85cc5c
farishuric <faris_huric@hotmail.com>
2023-09-17 21:53:36 +0200

    Initial commit

---
+++ Troshko/TroshkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TroshkoApp.swift
+//  Troshko
+//
+//  Created by Faris Hurić on 17. 9. 2023..
+//
+
+import SwiftUI
+
+@main
+struct TroshkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
