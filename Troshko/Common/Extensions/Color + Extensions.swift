44934bd3dd68455c446593d53ecfd831138601b1
farishuric <faris_huric@hotmail.com>
2023-09-25 09:50:40 +0200

    UI updates

---
+++ Troshko/Common/Extensions/Color + Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Color + Extensions.swift
+//  Troshko
+//
+//  Created by Faris Hurić on 25. 9. 2023..
+//
+
+import Foundation
+import SwiftUI
+import DGCharts
+
+extension Color {
+
+    static func getChartColors() -> [NSUIColor] {
+        let colors: [NSUIColor] = [
+            NSUIColor(.red),
+            NSUIColor(.blue),
+            NSUIColor(.green),
+            NSUIColor(.yellow),
+            NSUIColor(.orange),
+            NSUIColor(.purple),
+            NSUIColor(.indigo),
+            NSUIColor(.pink),
+        ]
+        
+        return colors
+    }
+}
